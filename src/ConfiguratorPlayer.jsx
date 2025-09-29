bcd8c5652ff11478da503fb4e6467547b6aaa0e1
Juan Maldini <juan.maldini@outlook.com>
2025-09-29 16:04:21 -0300

    Update iframe source and attributes in ConfiguratorPlayer
    Replaces the iframe's src URL with a new Vagon stream link, updates the id to 'vagonFrame', and adds allow permissions for microphone, clipboard, encrypted media, and fullscreen. Also refines the style attributes for height and width.

--- src/ConfiguratorPlayer.jsx
+++ src/ConfiguratorPlayer.jsx
@@ -3,15 +3,10 @@ import "./App.css";
 function ConfiguratorPlayer() {
   return (
     <div className="rooot">
-      <iframe
-      style={{ visibility: 'visible', border: 'none' }}
-      id='iframe_1'
-      src='https://connector.eagle3dstreaming.com/v6/eyJvd25lciI6IlZhbmlzaGluZ1BvaW50M0QiLCJhcHBOYW1lIjoiVkNvbmZpZ3VyYXRvciIsImNvbmZpZ05hbWUiOiJWQ29uZmlndXJhdG9yIn0='
-      height='100%'
-      width='100%'
-      allowfullscreen
-      title='host'
-      ></iframe>
+      <iframe id="vagonFrame"
+      style={{ visibility: 'visible', height: '100%', width: '100%', border: 'none' }}
+      allow="microphone  *; clipboard-read *; clipboard-write *; encrypted-media *; fullscreen *;"
+      src="https://streams.vagon.io/streams/156f3fce-185f-4bf9-9bc8-592352434fcd "/>
     </div>
   );
 }
